01db017779ccee3193beda32992907cc00f9357c
David Martín <dmartinblazquez@gmail.com>
2020-08-11 20:30:53 +0200

    Creación del Servicio Informacion

---
+++ src/app/informacion.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Post } from './models/post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InformacionService {
+
+  post: Post[];
+
+  constructor() { }
+}
+
+agregarPost() {
+  return this.post.push(Post);
+}
+
+getAllPost() {
+  return this.post[];
+}
